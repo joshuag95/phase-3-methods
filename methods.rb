b283be7272de21c9f21775ed84fe3fea4c45383e
Joshua Greer <jtgreer95@gmail.com>
2022-08-08 10:56:18 -0500

    commit tree e2c8b11c6c9fb696d4f7e8cc509431370f633e58

--- methods.rb
+++ methods.rb
@@ -1,1 +1,32 @@
+
 # Your code here!
+
+def greet_programmer 
+    puts "Hello Programmer"
+end
+
+def greet name
+    puts "Hello, #{name}"
+end
+
+
+def greet_with_default name="Programmer"
+    puts "Hello, #{name}"
+end
+
+def add num1, num2
+    num1 + num2
+end
+
+def halve num
+    if num != Integer
+        nil
+end
+       num / 2
+end
+
+greet_programmer
+greet Joshua
+greet_with_default
+add 4, 5
+halve 6
